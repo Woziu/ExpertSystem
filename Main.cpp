bd883a19d6c8297547a200b9d0e963fc8854756b
Marcin <marcin.woz@student.uksw.edu.pl>
2017-01-15 02:54:01 +0100

    Added all questions

--- Main.cpp
+++ Main.cpp
@@ -5,13 +5,39 @@
 int main()
 {
 	QuestionManager manager;
-	manager.addElement(Question((std::string)"purpose", std::string{ "Why do you want to learn?" }, { "For education", "To make money", "For my kids" }));
-	manager.addElement(Question((std::string)"anyIdea", std::string{ "Do you have a brilliant idea/platform in mind?" }, { "No. Just want to get started", "Yes" }));
+	manager.addElement(Question((std::string)"purpose", std::string{ "Why do you want to learn?" }, 
+		{ "For education", "To make money", "For my kids" }));
+	manager.addElement(Question((std::string)"anyIdea", std::string{ "Do you have a brilliant idea/platform in mind?" }, 
+		{ "No. Just want to get started", "Yes" }));
+	manager.addElement(Question((std::string)"learningWay", std::string{ "What way do you prefer to learn thins?" },
+		{ "Easy way", "The slightly harder way", "The really hard way (but easier to pick up other languages in the future)" }));
+	manager.addElement(Question((std::string)"carMode", std::string{ "Do you prefer auto or manual car?" },
+		{ "Auto", "Manual" }));
+	manager.addElement(Question((std::string)"moneyWay", std::string{ "How do you want to make money?" },
+		{ "I want to get a job", "I have a startup idea" }));
+	manager.addElement(Question((std::string)"startupField", std::string{ "Which platform/field?" },
+		{ "Web", "Enterprise", "Mobile", "3D/Gaming" }));
+	manager.addElement(Question((std::string)"realTime", std::string{ "Does your web app provides info in real-time, like Twitter?" },
+		{ "Yes", "No" }));
+	manager.addElement(Question((std::string)"mobileOS", std::string{ "Which OS?" },
+		{ "iOS", "Android" }));
+	manager.addElement(Question((std::string)"aboutMS", std::string{ "What do you think about Microsoft?" },
+		{ "I dont like it", "I`m a fan" }));
+	manager.addElement(Question((std::string)"somethingNew", std::string{ "Do you want to try something new with huge potential, but less mature?" },
+		{ "Yes", "No", "Not sure" }));
+	manager.addElement(Question((std::string)"toy", std::string{ "Which one is your favourite toy?" },
+		{ "Lego", "Play-Doh", "I have an old & ugly toy, but I love it so much" }));
+	manager.addElement(Question((std::string)"jobField", std::string{ "Which one is your favourite toy?" },
+		{ "Lego", "Play-Doh", "I have an old & ugly toy, but I love it so much" }));
+
+
+
+
 	while (!manager.isResultFound())
 	{
 		manager.askQuestion();
 		manager.fetchUserAnswer();
-		manager.getElementById(0).getAnswer();
+		//manager.getElementById(0).getAnswer();
 		manager.checkResult();
 		if (manager.isResultFound())
 		{
--- QuestionManager.cpp
+++ QuestionManager.cpp
@@ -2,7 +2,7 @@
 
 QuestionManager::QuestionManager()
 {
-	m_currentQuestion = 0;
+	m_currentQuestion = 9;
 	m_resultFound = false;
 }
 
@@ -62,7 +62,7 @@ void QuestionManager::setAnswer(int t_answerId)
 void QuestionManager::checkResult()
 {
 	std::string lastAnserw = m_questionVector[m_currentQuestion].getAnswer();
-	if (getCurrentQuestionTopic() == "puropse")
+	if (getCurrentQuestionTopic() == "purpose")
 	{
 		if (lastAnserw == "For my kids")
 		{
@@ -75,22 +75,235 @@ void QuestionManager::checkResult()
 		}
 		else if (lastAnserw == "To make money")
 		{
-			m_currentQuestion = 2;
+			selectNextQuestionByTopic(std::string("moneyWay"));
 		}
 	}
 	else if (getCurrentQuestionTopic() == "anyIdea")
 	{
 		if (lastAnserw == "No. Just want to get started")
 		{
-			m_resultFound = true;
-			m_resultText = "Start with Scratch, then move to Python";
+			selectNextQuestionByTopic(std::string("learningWay"));
 		}
 		else if (lastAnserw == "Yes")
 		{
-			m_currentQuestion = 1;
+			selectNextQuestionByTopic(std::string("startupField"));
 		}
 	}
-
+	else if (getCurrentQuestionTopic() == "moneyWay")
+	{
+		if (lastAnserw == "I want to get a job")
+		{
+			selectNextQuestionByTopic(std::string("jobField"));
+		}
+		else if (lastAnserw == "I have a startup idea")
+		{
+			selectNextQuestionByTopic(std::string("startupField"));
+		}
+	}
+	else if (getCurrentQuestionTopic() == "learningWay")
+	{
+		if (lastAnserw == "Easy way")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose Python";
+		}
+		else if (lastAnserw == "The really hard way (but easier to pick up other languages in the future)")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose C++";
+		}
+		else if (lastAnserw == "The slightly harder way")
+		{
+			selectNextQuestionByTopic(std::string("carMode"));
+		}
+	}
+	else if (getCurrentQuestionTopic() == "carMode")
+	{
+		if (lastAnserw == "Auto")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose Java";
+		}
+		else if (lastAnserw == "Manual")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose C";
+		}
+	}
+	else if (getCurrentQuestionTopic() == "startupField")
+	{
+		if (lastAnserw == "3D/Gaming")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose C++";
+		}
+		else if (lastAnserw == "Mobile")
+		{
+			selectNextQuestionByTopic(std::string("mobileOS"));
+		}
+		else if (lastAnserw == "Enterprise")
+		{
+			selectNextQuestionByTopic(std::string("aboutMS"));
+		}
+		else if (lastAnserw == "Web")
+		{
+			selectNextQuestionByTopic(std::string("realTime"));
+		}
+	}
+	else if (getCurrentQuestionTopic() == "realTime")
+	{
+		if (lastAnserw == "Yes")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose JavaScript";
+		}
+		else if (lastAnserw == "No")
+		{
+			selectNextQuestionByTopic(std::string("somethingNew"));
+		}
+	}
+	else if (getCurrentQuestionTopic() == "mobileOS")
+	{
+		if (lastAnserw == "iOS")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose Objective-C";
+		}
+		else if (lastAnserw == "Android")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose Java";
+		}
+	}
+	else if (getCurrentQuestionTopic() == "aboutMS")
+	{
+		if (lastAnserw == "I dont like it")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose Java";
+		}
+		else if (lastAnserw == "I`m a fan")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose C#";
+		}
+	}
+	else if (getCurrentQuestionTopic() == "somethingNew")
+	{
+		if (lastAnserw == "Yes")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose JavaScript";
+		}
+		else if (lastAnserw == "No")
+		{
+			selectNextQuestionByTopic(std::string("toy"));
+		}
+		else if (lastAnserw == "Not sure")
+		{
+			selectNextQuestionByTopic(std::string("toy"));
+		}
+	}
+	else if (getCurrentQuestionTopic() == "toy")
+	{
+		if (lastAnserw == "Lego")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose Python";
+		}
+		else if (lastAnserw == "Play-Doh")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose Ruby";
+		}
+		else if (lastAnserw == "I have an old & ugly toy, but I love it so much")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose PHP";
+		}
+	}
+	else if (getCurrentQuestionTopic() == "jobField")
+	{
+		if (lastAnserw == "3D/Gaming")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose C++";
+		}
+		else if (lastAnserw == "Mobile")
+		{
+			selectNextQuestionByTopic(std::string("mobileOS"));
+		}
+		else if (lastAnserw == "Enterprise")
+		{
+			selectNextQuestionByTopic(std::string("aboutMS"));
+		}
+		else if (lastAnserw == "Web")
+		{
+			selectNextQuestionByTopic(std::string("whichEnd"));
+		}
+		else if (lastAnserw == "Doesn`t matter, I just want to make money")
+		{
+			selectNextQuestionByTopic(std::string("dijkstra"));
+		}
+		else if (lastAnserw == "I want to work for big tech companies")
+		{
+			selectNextQuestionByTopic(std::string("techComp"));
+		}
+	}
+	else if (getCurrentQuestionTopic() == "dijkstra")
+	{
+		if (lastAnserw == "Yes")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose Java";
+		}
+		else if (lastAnserw == "No")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose Cobol";
+		}
+	}
+	else if (getCurrentQuestionTopic() == "techComp")
+	{
+		if (lastAnserw == "Facebook" || lastAnserw == "Google")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose Python";
+		}
+		else if (lastAnserw == "Apple")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose Objective-C";
+		}
+		else if (lastAnserw == "Microsoft")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose C#";
+		}
+	}
+	else if (getCurrentQuestionTopic() == "whichEnd")
+	{
+		if (lastAnserw == "Back-end (brain behind a website)")
+		{
+			selectNextQuestionByTopic(std::string("backendFIeld"));
+		}
+		else if (lastAnserw == "Front-end (web interface)")
+		{
+			m_resultFound = true;
+			m_resultText = "You should choose JavaScript";
+		}
+	}
+	else if (getCurrentQuestionTopic() == "backendFIeld")
+	{
+		if (lastAnserw == "Startup")
+		{
+			selectNextQuestionByTopic(std::string("somethingNew"));
+		}
+		else if (lastAnserw == "Corporate")
+		{
+			selectNextQuestionByTopic(std::string("aboutMS"));
+		}
+	}
 }
 
 void QuestionManager::selectNextQuestionByTopic(std::string t_topic)
@@ -98,7 +311,7 @@ void QuestionManager::selectNextQuestionByTopic(std::string t_topic)
 	int i = 0;
 	for (auto it = m_questionVector.begin(); it != m_questionVector.end(); it++)
 	{
-		if (it->getTopic == t_topic)
+		if (it->getTopic() == t_topic)
 		{
 			m_currentQuestion = i;
 			break;
